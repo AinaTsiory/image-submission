5358f1b271c9f48b91b7520f070551c49ccd927e
tsiory <hei.tsiory.234@gmail.com>
2026-09-16 08:31:10 +0300

    feat(worker): resize thumbnail, update submission, send email

--- src/main/java/com/example/demo/service/event/ThumbnailRequestedService.java
+++ src/main/java/com/example/demo/service/event/ThumbnailRequestedService.java
@@ -21,48 +21,48 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ThumbnailRequestedService implements Consumer<ThumbnailRequested> {
 
-    private static final int THUMBNAIL_SIZE = 256;
+  private static final int THUMBNAIL_SIZE = 256;
 
-    private final BucketComponent bucketComponent;
-    private final SubmissionRepository submissionRepository;
-    private final Mailer mailer;
+  private final BucketComponent bucketComponent;
+  private final SubmissionRepository submissionRepository;
+  private final Mailer mailer;
 
-    @SneakyThrows
-    @Override
-    public void accept(ThumbnailRequested event) {
-        var originalFile = bucketComponent.download(event.getOriginalKey());
-        var format = event.getOriginalKey().endsWith(".png") ? "png" : "jpg";
+  @SneakyThrows
+  @Override
+  public void accept(ThumbnailRequested event) {
+    var originalFile = bucketComponent.download(event.getOriginalKey());
+    var format = event.getOriginalKey().endsWith(".png") ? "png" : "jpg";
 
-        var sourceImage = ImageIO.read(originalFile);
-        var imageType = format.equals("png") ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB;
-        var thumbnailImage = new BufferedImage(THUMBNAIL_SIZE, THUMBNAIL_SIZE, imageType);
-        Graphics2D graphics = thumbnailImage.createGraphics();
-        graphics.drawImage(sourceImage, 0, 0, THUMBNAIL_SIZE, THUMBNAIL_SIZE, null);
-        graphics.dispose();
+    var sourceImage = ImageIO.read(originalFile);
+    var imageType = format.equals("png") ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB;
+    var thumbnailImage = new BufferedImage(THUMBNAIL_SIZE, THUMBNAIL_SIZE, imageType);
+    Graphics2D graphics = thumbnailImage.createGraphics();
+    graphics.drawImage(sourceImage, 0, 0, THUMBNAIL_SIZE, THUMBNAIL_SIZE, null);
+    graphics.dispose();
 
-        var thumbnailFile = File.createTempFile("thumbnail-" + event.getSubmissionId(), "." + format);
-        ImageIO.write(thumbnailImage, format, thumbnailFile);
+    var thumbnailFile = File.createTempFile("thumbnail-" + event.getSubmissionId(), "." + format);
+    ImageIO.write(thumbnailImage, format, thumbnailFile);
 
-        var thumbnailKey = "thumbnails/" + event.getSubmissionId() + "." + format;
-        bucketComponent.upload(thumbnailFile, thumbnailKey);
+    var thumbnailKey = "thumbnails/" + event.getSubmissionId() + "." + format;
+    bucketComponent.upload(thumbnailFile, thumbnailKey);
 
-        submissionRepository
-                .findById(event.getSubmissionId())
-                .ifPresent(
-                        submission -> {
-                            submission.setThumbnailKey(thumbnailKey);
-                            submissionRepository.save(submission);
-                        });
+    submissionRepository
+        .findById(event.getSubmissionId())
+        .ifPresent(
+            submission -> {
+              submission.setThumbnailKey(thumbnailKey);
+              submissionRepository.save(submission);
+            });
 
-        var downloadLink = bucketComponent.presign(thumbnailKey, Duration.ofDays(7)).toString();
+    var downloadLink = bucketComponent.presign(thumbnailKey, Duration.ofDays(7)).toString();
 
-        mailer.accept(
-                new Email(
-                        new InternetAddress(event.getEmail()),
-                        List.of(),
-                        List.of(),
-                        "Your thumbnail is ready",
-                        "Your thumbnail has been generated. Download it here: " + downloadLink,
-                        List.of()));
-    }
+    mailer.accept(
+        new Email(
+            new InternetAddress(event.getEmail()),
+            List.of(),
+            List.of(),
+            "Your thumbnail is ready",
+            "Your thumbnail has been generated. Download it here: " + downloadLink,
+            List.of()));
+  }
 }
